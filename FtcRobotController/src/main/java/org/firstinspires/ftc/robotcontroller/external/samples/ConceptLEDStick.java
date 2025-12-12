84fb56d2e05f2c00c1f4de57f86ab27c28b04386
Harry <harryseymour.ulster@gmail.com>
2025-12-12 15:30:12 +0000

    Disco and that

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptLEDStick.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptLEDStick.java
@@ -1,36 +1,4 @@
 package org.firstinspires.ftc.robotcontroller.external.samples;
-/*
-        Copyright (c) 2021-24 Alan Smith
-
-        All rights reserved.
-
-        Redistribution and use in source and binary forms, with or without modification,
-        are permitted (subject to the limitations in the disclaimer below) provided that
-        the following conditions are met:
-
-        Redistributions of source code must retain the above copyright notice, this list
-        of conditions and the following disclaimer.
-
-        Redistributions in binary form must reproduce the above copyright notice, this
-        list of conditions and the following disclaimer in the documentation and/or
-        other materials provided with the distribution.
-
-        Neither the name of Alan Smith nor the names of its contributors may be used to
-        endorse or promote products derived from this software without specific prior
-        written permission.
-
-        NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-        LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-        "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-        THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
-        ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-        FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-        DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-        SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-        CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-        TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-        THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
 
 import android.graphics.Color;
 
@@ -71,7 +39,7 @@ public class ConceptLEDStick extends OpMode {
 
     @Override
     public void init() {
-        ledStick = hardwareMap.get(SparkFunLEDStick.class, "back_leds");
+        ledStick = hardwareMap.get(SparkFunLEDStick.class, "back_LEDs");
         ledStick.setBrightness(brightness);
         ledStick.setColor(Color.GREEN);
     }
@@ -87,7 +55,7 @@ public class ConceptLEDStick extends OpMode {
         telemetry.addLine("Hold the B button to turn red");
         telemetry.addLine("Hold the left bumper to turn off");
         telemetry.addLine("Use DPAD Up/Down to change brightness");
-        
+
         if (getRuntime() > END_GAME_TIME) {
             int[] ledColors = {Color.RED, Color.YELLOW, Color.RED, Color.YELLOW, Color.RED,
                     Color.YELLOW, Color.RED, Color.YELLOW, Color.RED, Color.YELLOW};
